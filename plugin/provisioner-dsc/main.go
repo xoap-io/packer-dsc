69bbf9d7bca58123e9503694f1ce752dd5e47ee4
Antun-Oreskovic <114746621+Antun-Oreskovic@users.noreply.github.com>
2023-10-10 11:05:27 +0200

    fix: updated for latest packer

--- plugin/provisioner-dsc/main.go
+++ plugin/provisioner-dsc/main.go
@@ -1,16 +1,25 @@
 package main
 
 import (
-	"github.com/mefellows/packer-dsc/provisioner/dsc"
-	"github.com/hashicorp/packer/packer/plugin"
+	"fmt"
+	"os"
+
+	"github.com/hashicorp/packer-plugin-sdk/plugin"
+	"github.com/xoap-io/packer-plugin-dsc/provisioner/dsc"
 )
 
 func main() {
-
-	server, err := plugin.Server()
+	pps := plugin.NewSet()
+	pps.RegisterProvisioner(plugin.DEFAULT_NAME, new(dsc.Provisioner))
+	err := pps.Run()
 	if err != nil {
-		panic(err)
+		fmt.Fprintln(os.Stderr, err.Error())
+
+		fmt.Fprintln(os.Stderr, "Atempting to run with internal arguments")
+		errCommand := pps.RunCommand("start", "provisioner", plugin.DEFAULT_NAME)
+		if errCommand != nil {
+			fmt.Fprintln(os.Stderr, errCommand.Error())
+			os.Exit(1)
+		}
 	}
-	server.RegisterProvisioner(new(dsc.Provisioner))
-	server.Serve()
 }
